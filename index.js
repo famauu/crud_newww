cbf06322667bafb389a0c27df5b7d747539ac3f1
Fadhil Maulana <fadhil.maulana596@gmail.com>
2024-04-22 23:20:32 +0700

    update

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const fastify = require('fastify')({
 const db = require('./connection');
 //=========================
 
-// 1. params -> nampilin detail buku (http://localhost:3000/books/100000)
+// 1. params -> nampilin list buku, bisa search by judul (http://localhost:3000/books?search=...)
 fastify.get('/books/:search', async (request, reply) => {
-    const books = await db.query("SELECT sku, judul from books where judul like $1", [
+    const books = await db.query("SELECT sku, judul FROM books WHERE sku LIKE $1", [
       `%${request.params.search}%`
       ]);
     return books;
   })
 
-// 2. querystring -> nampilin list buku, bisa search by judul (http://localhost:3000/books?search=...)
-fastify.get('/books/:idBooks', async (request, reply) => {
-    const books = await db.query("select * from books where id = $1", [
+// 2. querystring -> nampilin detail buku (http://localhost:3000/books)
+fastify.get('/books', async (request, reply) => {
+    const books = await db.query("SELECT * FROM books WHERE sku = $1", [
       `%${request.query.search}%`
       ]);
     return books;
@@ -25,7 +25,7 @@ fastify.get('/books/:idBooks', async (request, reply) => {
 
 // 3. payload (post) -> insert buku baru
 fastify.post('/books/:idBooks', async (request, reply) => {
-    const tambahBooks = request.body;
+    const addBooks = request.body;
     const books = await db.query("INSERT INTO books (sku, judul, harga, stock) VALUES ('${addBooks.sku}','${addBooks.judul}','${addBooks.harga}','${addBooks.stock}');")
     return ("berhasil ditambahkan");
   })
@@ -33,15 +33,15 @@ fastify.post('/books/:idBooks', async (request, reply) => {
 // 4. payload (put) -> update buku
 fastify.put('/books/:idBooks', async (request, reply) => {
     const updateBooks = request.body;
-    const books = await db.query("UPDATE books SET judul = '${updateBooks.judul}' where id = $1", [
+    const books = await db.query("UPDATE books SET judul = '${updateBooks.judul}' WHERE judul = $1", [
       `%${request.body.search}%`
       ]);
-    return ("berhasil diupdate" + request.params.idBooks);
+    return ("berhasil diupdate" + request.params.search);
   })
 
 // 5. delete (>< payload) -> hapus buku
 fastify.delete('/books/:search', async (request, reply) => {
-    const books = await db.query("delete from books where id = $1;", [
+    const books = await db.query("DELETE books WHERE sku = $1", [
       `%${request.body.search}%`
       ]);
     return ("behasil dihapus" + request.params.search);
